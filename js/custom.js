ce6144c05ad8648158c90c948be777f7e193980c
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-10-12 00:30:28 -0500

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+//var colors = [
+//  ['#D3B6C6', '#4B253A'], ['#FCE6A4', '#EFB917'], ['#BEE3F7', '#45AEEA'], ['#F8F9B6', '#D2D558'], ['#F4BCBF', '#D43A43']
+//], circles = [], percentage_values = [30,70,70];
+$(document).ready(function(){
+  var circles = [], percentage_values = [80,70,30,60], hasCreatedObjects = false;
+
+  for (var i = 1; i <= 4; i++) {
+    var child = document.getElementById('circles-' + i);
+
+    $('#circles-' + i).circliful({
+      animation: 1,
+      animationStep: 5,
+      foregroundBorderWidth:10,
+      backgroundBorderWidth:10,
+      fontColor: '#333',
+      textSize: 28,
+      textStyle: 'font-size: 14px;',
+      textColor: '#666',
+      foregroundColor: '#de3814',
+      multiPercentage: 1,
+      animateInView: 'true',
+      percent: percentage_values[i-1]
+    });
+  }
+//---------------------------------------
+var scroll_start = 0;
+var offset = 70;
+  $(document).scroll(function() {
+    scroll_start = $(this).scrollTop();
+    if(scroll_start > offset.top) {
+      $(".navbar-default").css('background-color', '#fff');
+      $('.navbar-default .navbar-nav > li > a ').css('color','#777');
+    } else if (scroll_start == 0) {
+      $('.navbar-default').css('background-color', 'rgba(248, 248, 248, 0.84)');
+    } else{
+      $(".navbar-default").css('background-color', '#fff');
+      $('.navbar-default .navbar-nav > li > a ').css('color','#777');
+    }
+  });
+
+});
